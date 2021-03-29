c0b759cd6e34b27a3604bf8549b9fc843b04d577
Oleg Nazarov <80001925+Nazarov-Oleg@users.noreply.github.com>
2021-03-29 10:50:13 +0300

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,9 +1,11 @@
 let number = 0;
 let taskResult;
 if (number % 3 == 0 && number % 5 !== 0) {
-  taskResult = 'Fizz'
+taskResult = 'Fizz';
 } else if (number % 5 == 0 && number % 3 !== 0) {
-taskResult = 'Buzz'
-}  else if (number % 3 == 0 && number % 5 == 0) {
-taskResult = 'FizzBuzz'
-}  else (taskResult = number)  
+taskResult = 'Buzz';
+} else if (number % 3 == 0 && number % 5 == 0) {
+taskResult = 'FizzBuzz';
+} else { 
+taskResult = number;
+}
